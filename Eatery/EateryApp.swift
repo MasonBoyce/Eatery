51675657afd63a066f2019d547cd4370801cb3bc
Mason Boyce <boyce.mason@gmail.com>
2021-06-25 15:50:25 -0400

    Initial Commit

---
+++ Eatery/EateryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EateryApp.swift
+//  Eatery
+//
+//  Created by Mason Boyce on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct EateryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
